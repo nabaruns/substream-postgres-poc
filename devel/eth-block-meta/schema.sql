f759999629005840bfbd569d514dc37a8c4d8b79
harshrjjpt <harshrjjpt@gmail.com>
2023-07-06 16:39:28 +0530

    poc-update

--- devel/eth-block-meta/schema.sql
+++ devel/eth-block-meta/schema.sql
@@ -1,6 +1,6 @@
 create table block_meta
 (
-    id          text not null constraint block_meta_pk primary key,
+    id          integer not null constraint block_meta_pk primary key,
     at          text,
     number      integer,
     hash        text,
@@ -25,7 +25,24 @@ create table transactions
 (
     id         text not null constraint transactions_pk primary key,
     status     text,
-    gas_used  bigint,
-    gas_limit   text,
-    hash       text
+    gas_used   bigint,
+    gas_limit  text,
+    block_number bigint,
+    timestamp  text,
+    hash       text,
+    to_address text,
+    from_address   text
 );
+
+create table contracts
+(
+    id         text not null constraint contracts_pk primary key,
+    block_number bigint,
+    owner        text,
+    address      text,
+    transaction_hash text,
+    timestamp      text
+
+);
+
+
--- docs/tutorial/src/lib.rs
+++ docs/tutorial/src/lib.rs
@@ -2,147 +2,81 @@ mod block_timestamp;
 mod pb;
 
 use self::block_timestamp::BlockTimestamp;
+use substreams_database_change::tables::Tables;
 use hex;    
-use pb::acme::{BlockMeta, BigInt, Transaction, TransactionList};
+use pb::acme::{BlockMeta, Transaction, TransactionList, Contract, ContractList};
 use substreams::store::{
     self, DeltaProto, StoreNew, StoreSetIfNotExists, StoreSetIfNotExistsProto,
 };
 use substreams::Hex;
+use prost_types::Value;
+use substreams_entity_change::pb::entity::EntityChanges;
 use substreams_ethereum::pb::eth::v2 as eth;
 use substreams_ethereum::pb::eth::v2::TransactionTraceStatus;
 use substreams_database_change::pb::database::{table_change::Operation, DatabaseChanges};
 use substreams_ethereum::pb as ethpb;
+use substreams_ethereum::pb::eth::v2::BigInt;
 
-// store for blocks data
-#[substreams::handlers::store]
-fn store_block_meta_start(blk: eth::Block, s: StoreSetIfNotExistsProto<BlockMeta>) {
-    let (timestamp, meta) = transform_block_to_block_meta(blk);
+// create block entity
+fn add_block_entity(tables: &mut Tables, blk: &eth::Block) {
+    let block_hash = Hex(&blk.hash).to_string();
+    let header = blk.header.as_ref().unwrap();
 
-    s.set_if_not_exists(meta.number, timestamp.start_of_day_key(), &meta);
-    s.set_if_not_exists(meta.number, timestamp.start_of_month_key(), &meta);
+    tables
+        .create_row("block_meta", base_64_to_hex(block_hash.clone()))
+        .set("id", blk.hash.clone())
+        .set("number", blk.number)
+        .set(
+            "parent_hash",
+            Hex(&blk.header.as_ref().unwrap().parent_hash).to_string(),
+        )
+        .set(
+            "timestamp",
+            blk.header.as_ref().unwrap().timestamp.as_ref().unwrap(),
+        )
+        .set("size", blk.size)
+        .set("parent_hash", header.parent_hash.clone())
+        // .set("transaction_nonce", header.nonce)
+        .set("receipt_root", header.receipt_root.clone())
+        .set("gas_limit", header.gas_limit)
+        .set("gas_used", header.gas_used);
 }
 
-// store for transaction data
-#[substreams::handlers::store]
-fn store_transaction_data(blk: eth::Block, s: StoreSetIfNotExistsProto<Transaction>) {
-    let transaction_details_list = blk
-        .transaction_traces
-        .clone().into_iter()
-        .filter(|trx| trx.status == TransactionTraceStatus::Succeeded as i32)
-        .map(|trx| transform_transaction_to_transaction_meta(trx, &blk))
-        .collect();
-
-    let main_list = TransactionList {
-        transaction_details_list,
-    };
-    let timestamp = BlockTimestamp::from_block(&blk);
-    for transaction in main_list.transaction_details_list {
-        s.set_if_not_exists(transaction.blockNumber, timestamp.start_of_day_key(), &transaction);
-        s.set_if_not_exists(transaction.blockNumber, timestamp.start_of_month_key(), &transaction);
-    }
-}
-
-// // store for conract data
-// #[substreams::handlers::store]
-// fn store_transaction_data(blk: eth::Block, s: StoreSetIfNotExistsProto<Transaction>) {
-//     let transaction_details_list = blk
-//         .transaction_traces
-//         .clone().into_iter()
-//         .filter(|trx| trx.status == TransactionTraceStatus::Succeeded as i32)
-//         .map(|trx| transform_contract_to_contract_meta(trx, &blk))
-//         .collect();
-
-//     let main_list = ContractList {
-//         contract_list,
-//     };
-//     let timestamp = BlockTimestamp::from_block(&blk);
-//     for contract in main_list.contract_list {
-//         s.set_if_not_exists(contract.blockNumber, timestamp.start_of_day_key(), &contract);
-//         s.set_if_not_exists(contract.blockNumber, timestamp.start_of_month_key(), &contract);
-//     }
-// }
-
-
-// main db_out function
-#[substreams::handlers::map]
-fn db_out(
-    block_meta_start: store::Deltas<DeltaProto<BlockMeta>>,
-    store_transaction_data: store::Deltas<DeltaProto<Transaction>>
-) -> Result<DatabaseChanges, substreams::errors::Error> {
-    let mut database_changes: DatabaseChanges = Default::default();
-    transform_block_meta_to_database_changes(&mut database_changes, block_meta_start);
-    transform_transaction_data_to_database_changes(&mut database_changes, store_transaction_data);
-    Ok(database_changes)
-}
-
-fn transform_block_to_block_meta(blk: ethpb::eth::v2::Block) -> (BlockTimestamp, BlockMeta) {
-    let timestamp = BlockTimestamp::from_block(&blk);
-    let header = blk.header.unwrap();
-
-    (
-        timestamp,
-        BlockMeta {
-            id: base_64_to_hex(blk.hash.clone()),
-            timestamp: Some(header.timestamp.unwrap()),
-            parentHash: base_64_to_hex(header.parent_hash),
-            uncleHash: base_64_to_hex(header.uncle_hash),
-            receiptRoot: header.receipt_root.clone(),
-            gasLimit: header.gas_limit,
-            hash: base_64_to_hex(blk.hash.clone()),
-            gasUsed: header.gas_used,
-            number: blk.number,
-            nonce: header.nonce,
-            size: blk.size,
-        },
-    )
-}
-
-fn transform_transaction_to_transaction_meta(trx:  eth::TransactionTrace, block: &eth::Block) -> Transaction {
-    let header = block.header.as_ref().unwrap();
-    let block_number = block.number;
-    let time_stamp = header.timestamp.clone().unwrap().seconds;
+//create transaction entity
+fn add_trx_info_entity(tables: &mut Tables, trx: &eth::TransactionTrace,  block_number: &u64,
+    time_stamp: i64,) {
     
-        Transaction {
-            id:  base_64_to_hex(trx.hash),
-        gasUsed: trx.gas_used,
-        status: trx.status.to_string(),
-        index: trx.index,
-        nonce: trx.nonce,
-        // maxFeePerGas: option_bigint_to_number_string(trx.max_fee_per_gas),
-        // maxPriorityFeePerGas: option_bigint_to_number_string(trx.max_priority_fee_per_gas),
-        gasLimit: trx.gas_limit,
-        to: base_64_to_hex(trx.to),
-        from: base_64_to_hex(trx.from),
-        // value: option_bigint_to_number_string(trx.value),
-        blockNumber: block_number,
-        timestamp: time_stamp,
-        }
-    
+    tables
+        .create_row("transactions",  base_64_to_hex(trx.hash.clone()))
+        .set("id", trx.hash.clone())
+        .set("status", trx.status)
+        .set("gas_used",  trx.gas_used)
+        .set("gas_limit",  trx.gas_limit)
+        // .set("transaction_nonce",  trx.nonce)
+        .set("to_address",  base_64_to_hex(trx.to.clone()))
+        .set("from_address",  base_64_to_hex(trx.from.clone()))
+        .set("max_fee_per_gas",  option_bigint_to_number_string(trx.max_fee_per_gas.clone()))
+        // .set("max_priority_fee_per_gas",  option_bigint_to_number_string(trx.max_priority_fee_per_gas.clone()))
+        .set("block_number",  block_number.clone())
+        // .set("value",  option_bigint_to_number_string(trx.value.clone()))
+        .set("timestamp",  time_stamp);
 }
 
-// fn transform_contract_to_contract_meta(
-//     trx: eth::TransactionTrace,
-//     block: &eth::Block,
-// ) -> Option<Contract> {
-//     let header = block.header.as_ref().unwrap();
-//     let contract_check = String::from_utf8_lossy(&trx.input).to_string();
-//     let block_number = block.number;
-//     let time_stamp = header.timestamp.clone().unwrap().seconds;
-//     if contract_check.starts_with("`�`@R") {
-//         Some(Contract {
-//             owner: base_64_to_hex(trx.from),
-//             address: base_64_to_hex(trx.to),
-//             transactionHash:  base_64_to_hex(trx.hash),
-//             blockNumber:block_number,
-//             timestamp: time_stamp,
-//         })
-//     } else {    
-//         None
-//     }
-// }
+//create contract entity
+fn add_contracts_info_entity(tables: &mut Tables, trx: &eth::TransactionTrace,  block_number: &u64,
+    time_stamp: i64,) {
+    
+    tables
+        .create_row("contracts",  base_64_to_hex(trx.hash.clone()))
+        .set("id", trx.hash.clone())
+        .set("transaction_hash", trx.hash.clone())
+        .set("address",  base_64_to_hex(trx.to.clone()))
+        .set("owner",  base_64_to_hex(trx.from.clone()))
+        .set("block_number",  block_number.clone())
+        .set("timestamp",  time_stamp);
+}
 
-
-// base64 to hex
+//base64 to hex
 fn base_64_to_hex<T: std::convert::AsRef<[u8]>>(num:T) -> String {
     let num = hex::encode(&num);
     let num = num.to_string();
@@ -155,220 +89,37 @@ fn option_bigint_to_number_string(bigint: Option<BigInt>) -> String {
         .map(|num| {
             let bytes = num.bytes;
             let mut value: u128 = 0;
+
             for byte in bytes {
                 value = (value << 8) + u128::from(byte);
             }
+
             value.to_string()
         })
         .unwrap_or_else(String::new)
 }
 
-fn transform_block_meta_to_database_changes(
-    changes: &mut DatabaseChanges,
-    deltas: store::Deltas<DeltaProto<BlockMeta>>,
-) {
-    use substreams::pb::substreams::store_delta::Operation;
-
-    for delta in deltas.deltas {
-        match delta.operation {
-            Operation::Create => push_create(
-                changes,
-                &delta.key,
-                BlockTimestamp::from_key(&delta.key),
-                delta.ordinal,
-                delta.new_value,
-            ),
-            Operation::Update => push_update(
-                changes,
-                &delta.key,
-                delta.ordinal,
-                delta.old_value,
-                delta.new_value,
-            ),
-            Operation::Delete => todo!(),
-            x => panic!("unsupported opeation {:?}", x),
+ // main db_out function
+#[substreams::handlers::map]
+fn db_out(
+    blk: eth::Block
+) -> Result<DatabaseChanges, substreams::errors::Error> {
+    let header = &blk.header.as_ref().unwrap();
+    let block_number = &blk.number;
+    let time_stamp = header.timestamp.clone().unwrap().seconds;
+    let mut tables = Tables::new();
+    // get blocks data
+    add_block_entity(&mut tables, &blk);
+    for trx in &blk.transaction_traces{
+        // get transactions data
+        add_trx_info_entity(&mut tables, &trx, block_number, time_stamp);
+        let contract_check = String::from_utf8_lossy(&trx.input).to_string();
+        if contract_check.starts_with("`�`@R"){
+            //get contracts data
+            add_contracts_info_entity(&mut tables, &trx, block_number, time_stamp);
         }
+       
     }
+    Ok(tables.to_database_changes())
 }
 
-fn transform_transaction_data_to_database_changes(
-    changes: &mut DatabaseChanges,
-    deltas: store::Deltas<DeltaProto<Transaction>>,
-) {
-    use substreams::pb::substreams::store_delta::Operation;
-
-    for delta in deltas.deltas {
-        match delta.operation {
-            Operation::Create => push_create_transaction(
-                changes,
-                &delta.key,
-                delta.ordinal,
-                delta.new_value,
-            ),
-            Operation::Update => push_update_transaction(
-                changes,
-                &delta.key,
-                delta.ordinal,
-                delta.old_value,
-                delta.new_value,
-            ),
-            Operation::Delete => todo!(),
-            x => panic!("unsupported opeation {:?}", x),
-        }
-    }
-}
-
-// fn transform_contract_data_to_database_changes(
-//     changes: &mut DatabaseChanges,
-//     deltas: store::Deltas<DeltaProto<Contract>>,
-// ) {
-//     use substreams::pb::substreams::store_delta::Operation;
-
-//     for delta in deltas.deltas {
-//         match delta.operation {
-//             Operation::Create => push_create_contract(
-//                 changes,
-//                 &delta.key,
-//                 delta.ordinal,
-//                 delta.new_value,
-//             ),
-//             Operation::Update => push_update_contract(
-//                 changes,
-//                 &delta.key,
-//                 delta.ordinal,
-//                 delta.old_value,
-//                 delta.new_value,
-//             ),
-//             Operation::Delete => todo!(),
-//             x => panic!("unsupported opeation {:?}", x),
-//         }
-//     }
-// }
-
-// consider moving back into a standalone file
-//#[path = "db_out.rs"]
-//mod db;
-fn push_create(
-    changes: &mut DatabaseChanges,
-    key: &str,
-    timestamp: BlockTimestamp,
-    ordinal: u64,
-    value: BlockMeta,
-) {
-    changes
-        .push_change("block_meta", key, ordinal, Operation::Create)
-        .change("at", (None, timestamp))
-        .change("size", (None, value.size))
-        .change("number", (None, value.number))
-        .change("gas_limit", (None, value.gasLimit))
-        .change("gas_used", (None, value.gasUsed))
-        .change("id", (None, value.id))
-        .change("hash", (None, value.hash))
-        .change("uncle_hash", (None, value.uncleHash))
-        .change("receipt_root", (None, value.receiptRoot))
-        .change("parent_hash", (None, value.parentHash))
-        .change("timestamp", (None, value.timestamp.unwrap()));
-}
-
-fn push_update(
-    changes: &mut DatabaseChanges,
-    key: &str,
-    ordinal: u64,
-    old_value: BlockMeta,
-    new_value: BlockMeta,
-) {
-    changes
-        .push_change("block_meta", key, ordinal, Operation::Update)
-        .change("number", (old_value.number, new_value.number))
-        .change("size", (old_value.size, new_value.size))
-        .change("gas_limit", (old_value.number, new_value.gasLimit))
-        .change("gas_used", (old_value.number, new_value.gasUsed))
-        .change("id", (old_value.id, new_value.id));
-        // .change("hash", (old_value.hash, new_value.hash))
-        // .change(
-        //     "parent_hash",
-        //     (old_value.parentHash, new_value.parentHash),
-        // )
-        // .change(
-        //     "uncle_hash",
-        //     (old_value.uncleHash, new_value.uncleHash),
-        // )
-        // .change(
-        //     "timestamp",
-        //     (&old_value.timestamp.unwrap(), &new_value.timestamp.unwrap()),
-        // );
-}
-
-
-fn push_create_transaction(
-    changes: &mut DatabaseChanges,
-    key: &str,
-    ordinal: u64,
-    value: Transaction,
-) {
-    changes
-        .push_change("transactions", key, ordinal, Operation::Create)
-        .change("status", (None, value.status))
-        .change("gas_limit", (None, value.gasLimit))
-        .change("gas_used", (None, value.gasUsed))
-        .change("id", (None, value.id.clone()))
-        .change("block_number", (None, value.blockNumber))
-        .change("timestamp", (None, value.timestamp))
-        .change("hash", (None, value.id.clone()))
-        .change("to_address", (None, value.to))
-        .change("from_address", (None, value.from));
-}
-
-
-fn push_update_transaction(
-    changes: &mut DatabaseChanges,
-    key: &str,
-    ordinal: u64,
-    old_value: Transaction,
-    new_value: Transaction,
-) {
-    changes
-        .push_change("transactions", key, ordinal, Operation::Update)
-        .change("status", (old_value.status, new_value.status))
-        .change("gas_limit", (old_value.gasLimit, new_value.gasLimit))
-        .change("gas_used", (old_value.gasUsed, new_value.gasUsed))
-        .change("id", (old_value.id.clone(), new_value.id.clone()))
-        .change("block_number", (old_value.blockNumber, new_value.blockNumber))
-        .change("hash", (old_value.id.clone(), new_value.id.clone()))
-        .change("to_address", (old_value.to, new_value.to))
-        .change("from_address", (old_value.from, new_value.from));
-}
-
-// fn push_create_contract(
-//     changes: &mut DatabaseChanges,
-//     key: &str,
-//     ordinal: u64,
-//     value: Contract,
-// ) {
-//     changes
-//         .push_change("contracts", key, ordinal, Operation::Create)
-//         .change("owner", (None, value.owner))
-//         .change("address", (None, value.address))
-//         .change("transaction_hash", (None, value.transactionHash))
-//         .change("block_number", (None, value.blockNumber))
-//         .change("timestamp", (None, value.timestamp));
-// }
-
-// fn push_update_contract(
-//     changes: &mut DatabaseChanges,
-//     key: &str,
-//     ordinal: u64,
-//     old_value: Contract,
-//     new_value: Contract,
-// ) {
-//     changes
-//         .push_change("contracts", key, ordinal, Operation::Update)
-//         .change("status", (old_value.status, new_value.status))
-//         .change("gas_limit", (old_value.gasLimit, new_value.gasLimit))
-//         .change("gas_used", (old_value.gasUsed, new_value.gasUsed))
-//         .change("id", (old_value.id.clone(), new_value.id.clone()))
-//         .change("block_number", (old_value.blockNumber, new_value.blockNumber))
-//         .change("hash", (old_value.id.clone(), new_value.id.clone()))
-//         .change("to_address", (old_value.to, new_value.to))
-//         .change("from_address", (old_value.from, new_value.from));
-// }
